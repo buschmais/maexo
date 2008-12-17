04729a3f653ab52aadee98884c975a4c99d5c156
dirk.mahler@buschmais.com <dirk.mahler@buschmais.com@edd260d2-c1fc-11dd-87c2-1b6a6bee664f>
2008-12-17 20:46:26 +0000

    Complete - task 4: copyright headers are missing http://code.google.com/p/maexo/issues/detail?id=4

--- maexo/mbeans/osgi-core-mbeans/src/main/java/com/buschmais/osgi/maexo/mbeans/osgi/core/impl/listener/EventListener.java
+++ maexo/mbeans/osgi-core-mbeans/src/main/java/com/buschmais/osgi/maexo/mbeans/osgi/core/impl/listener/EventListener.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2008 buschmais GbR
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
 package com.buschmais.osgi.maexo.mbeans.osgi.core.impl.listener;
 
 import java.util.Dictionary;
@@ -38,8 +54,8 @@ public abstract class EventListener {
 	}
 
 	/**
-	 * Registers a managed bean as service and stores it under a provided key for
-	 * later unregistration
+	 * Registers a managed bean as service and stores it under a provided key
+	 * for later unregistration
 	 * 
 	 * @param mbeanInterface
 	 *            the interface to use for registration
