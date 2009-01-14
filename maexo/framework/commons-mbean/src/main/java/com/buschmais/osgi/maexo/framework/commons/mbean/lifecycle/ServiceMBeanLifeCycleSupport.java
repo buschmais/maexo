f6ece88acbbc2b1d1c561db3599991d46d05e6c2
frank.schwarz@buschmais.com <frank.schwarz@buschmais.com@edd260d2-c1fc-11dd-87c2-1b6a6bee664f>
2009-01-14 19:09:17 +0000

    methods reordered to make it more readable

--- maexo/framework/commons-mbean/src/main/java/com/buschmais/osgi/maexo/framework/commons/mbean/lifecycle/ServiceMBeanLifeCycleSupport.java
+++ maexo/framework/commons-mbean/src/main/java/com/buschmais/osgi/maexo/framework/commons/mbean/lifecycle/ServiceMBeanLifeCycleSupport.java
@@ -45,116 +45,6 @@ public abstract class ServiceMBeanLifeCycleSupport extends
 	}
 
 	/**
-	 * Starts the life cycle listener.
-	 * <p>
-	 * The existing services are looked up to be published as mbeans. Later the
-	 * life cycle listener is registered with the bundle context.
-	 */
-	public final void start() {
-		// register all existing services as mbeans
-		ServiceReference[] serviceReferences = this.getServices();
-		if (serviceReferences != null) {
-			for (ServiceReference serviceReference : serviceReferences) {
-				this.serviceChanged(new ServiceEvent(ServiceEvent.REGISTERED,
-						serviceReference));
-			}
-		}
-		try {
-			// add the service listener
-			super.getBundleContext().addServiceListener(this, this.getFilter());
-		} catch (InvalidSyntaxException e) {
-			throw new IllegalStateException(
-					"cannot add service listener to bundle context using service filter "
-							+ this.getFilter(), e);
-		}
-	}
-
-	/**
-	 * Stops the life cycle listener.
-	 * <p>
-	 * The existing services are looked up, their mbeans get unpublished and the
-	 * life cycle listener is unregistered from the bundle context.
-	 */
-	public final void stop() {
-		// remove service listener
-		super.getBundleContext().removeServiceListener(this);
-		// unregister all registered service mbeans
-		ServiceReference[] serviceReferences = this.getServices();
-		if (serviceReferences != null) {
-			for (ServiceReference serviceReference : serviceReferences) {
-				this.serviceChanged(new ServiceEvent(
-						ServiceEvent.UNREGISTERING, serviceReference));
-			}
-		}
-	}
-
-	/**
-	 * Returns the currently registered service references for the interface
-	 * provided by {@link #getServiceInterface()}.
-	 * 
-	 * @return the service references
-	 */
-	private ServiceReference[] getServices() {
-		try {
-			return super.getBundleContext().getServiceReferences(null,
-					this.getFilter());
-		} catch (InvalidSyntaxException e) {
-			throw new IllegalStateException(
-					"cannot get services from bundle context using service filter "
-							+ this.getFilter(), e);
-		}
-	}
-
-	/**
-	 * Returns a filter using the interface from {@link #getServiceInterface()}
-	 * as object class and the service filter obtained from
-	 * {@link #getServiceFilter()}.
-	 * 
-	 * @return the filter or <code>null</code> if there is no service interface
-	 *         declared
-	 */
-	private String getFilter() {
-		Class<?> serviceInterface = this.getServiceInterface();
-		String serviceFilter = this.getServiceFilter();
-		StringBuilder filter = new StringBuilder();
-		if (serviceInterface != null) {
-			filter.append(String.format("(objectClass=%s)", serviceInterface
-					.getName()));
-		}
-		if (serviceFilter != null) {
-			filter.append(serviceFilter);
-		}
-		if (filter.length() == 0) {
-			return null;
-		}
-		return String.format("(&%s)", filter);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public final synchronized void serviceChanged(ServiceEvent serviceEvent) {
-		ServiceReference serviceReference = serviceEvent.getServiceReference();
-		Object service = super.getBundleContext().getService(serviceReference);
-		ObjectName objectName = this.getObjectName(serviceReference, service);
-		switch (serviceEvent.getType()) {
-		case ServiceEvent.REGISTERED:
-			super.registerMBeanService(this.getMBeanInterface(), objectName,
-					this.getMBean(serviceReference, service));
-			break;
-		case ServiceEvent.UNREGISTERING:
-			try {
-				super.unregisterMBeanService(objectName);
-			} finally {
-				super.getBundleContext().ungetService(serviceReference);
-			}
-			break;
-		default:
-			assert false : "Unexpected ServiceEvent";
-		}
-	}
-
-	/**
 	 * Returns the service interface supported by the life cycle event listener.
 	 * 
 	 * @return the service interface
@@ -203,5 +93,115 @@ public abstract class ServiceMBeanLifeCycleSupport extends
 	 * @return the mbean interface
 	 */
 	protected abstract Class<?> getMBeanInterface();
+
+	/**
+	 * Returns a filter using the interface from {@link #getServiceInterface()}
+	 * as object class and the service filter obtained from
+	 * {@link #getServiceFilter()}.
+	 * 
+	 * @return the filter or <code>null</code> if there is no service interface
+	 *         declared
+	 */
+	private String getFilter() {
+		Class<?> serviceInterface = this.getServiceInterface();
+		String serviceFilter = this.getServiceFilter();
+		StringBuilder filter = new StringBuilder();
+		if (serviceInterface != null) {
+			filter.append(String.format("(objectClass=%s)", serviceInterface
+					.getName()));
+		}
+		if (serviceFilter != null) {
+			filter.append(serviceFilter);
+		}
+		if (filter.length() == 0) {
+			return null;
+		}
+		return String.format("(&%s)", filter);
+	}
+
+	/**
+	 * Returns the currently registered service references for the interface
+	 * provided by {@link #getServiceInterface()}.
+	 * 
+	 * @return the service references
+	 */
+	private ServiceReference[] getServices() {
+		try {
+			return super.getBundleContext().getServiceReferences(null,
+					this.getFilter());
+		} catch (InvalidSyntaxException e) {
+			throw new IllegalStateException(
+					"cannot get services from bundle context using service filter "
+							+ this.getFilter(), e);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public final synchronized void serviceChanged(ServiceEvent serviceEvent) {
+		ServiceReference serviceReference = serviceEvent.getServiceReference();
+		Object service = super.getBundleContext().getService(serviceReference);
+		ObjectName objectName = this.getObjectName(serviceReference, service);
+		switch (serviceEvent.getType()) {
+		case ServiceEvent.REGISTERED:
+			super.registerMBeanService(this.getMBeanInterface(), objectName,
+					this.getMBean(serviceReference, service));
+			break;
+		case ServiceEvent.UNREGISTERING:
+			try {
+				super.unregisterMBeanService(objectName);
+			} finally {
+				super.getBundleContext().ungetService(serviceReference);
+			}
+			break;
+		default:
+			assert false : "Unexpected ServiceEvent";
+		}
+	}
+
+	/**
+	 * Starts the life cycle listener.
+	 * <p>
+	 * The existing services are looked up to be published as mbeans. Later the
+	 * life cycle listener is registered with the bundle context.
+	 */
+	public final void start() {
+		// register all existing services as mbeans
+		ServiceReference[] serviceReferences = this.getServices();
+		if (serviceReferences != null) {
+			for (ServiceReference serviceReference : serviceReferences) {
+				this.serviceChanged(new ServiceEvent(ServiceEvent.REGISTERED,
+						serviceReference));
+			}
+		}
+		try {
+			// add the service listener
+			super.getBundleContext().addServiceListener(this, this.getFilter());
+		} catch (InvalidSyntaxException e) {
+			throw new IllegalStateException(
+					"cannot add service listener to bundle context using service filter "
+							+ this.getFilter(), e);
+		}
+	}
+
+	/**
+	 * Stops the life cycle listener.
+	 * <p>
+	 * The existing services are looked up, their mbeans get unpublished and the
+	 * life cycle listener is unregistered from the bundle context.
+	 */
+	public final void stop() {
+		// remove service listener
+		super.getBundleContext().removeServiceListener(this);
+		// unregister all registered service mbeans
+		ServiceReference[] serviceReferences = this.getServices();
+		if (serviceReferences != null) {
+			for (ServiceReference serviceReference : serviceReferences) {
+				this.serviceChanged(new ServiceEvent(
+						ServiceEvent.UNREGISTERING, serviceReference));
+			}
+		}
+	}
 
 }
