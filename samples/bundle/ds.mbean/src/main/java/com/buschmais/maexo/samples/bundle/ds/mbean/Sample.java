cfa6f69c2d99d551df1344de5b59493f06929dc1
dirk.mahler@buschmais.com <dirk.mahler@buschmais.com@edd260d2-c1fc-11dd-87c2-1b6a6bee664f>
2009-02-10 17:29:11 +0000

    fix last commit (a class from the ds sample was missing)

---
+++ samples/bundle/ds.mbean/src/main/java/com/buschmais/maexo/samples/bundle/ds/mbean/Sample.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2009 buschmais GbR
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+package com.buschmais.maexo.samples.bundle.ds.mbean;
+
+/**
+ * The implementation of the Sample MBean interface.
+ */
+public class Sample implements SampleMBean {
+
+	/**
+	 * The attribute.
+	 */
+	private String attribute;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getAttribute() {
+		return this.attribute;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void setAttribute(String attribute) {
+		this.attribute = attribute;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public int operation(int a, int b) {
+		return a + b;
+	}
+}
